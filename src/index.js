54fb633ef958fb5a01e0c59b97bc3cf16c043015
Juan David Y <juand8012@gmail.com>
2021-04-08 15:55:45 -0500

    Fixing issues while bill were added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const button = document.getElementById('mainButton');
 const familyMembersContainer = document.querySelector('.mainFamilyContainer');
 const familyMembersButton = document.getElementById('familyMembersButton');
 const familyMembersGenre = document.querySelector('.familyMemberGenre');
-const familyMembersAge = document.getElementById('familyMemberAge').value;
+const familyMembersAge = document.getElementById('familyMemberAge');
 const totalPrice = document.querySelector('.rightSide');
 
 //Create another bill
@@ -18,13 +18,11 @@ familyMembersButton.addEventListener('click', addFamilyMembers);
 //Total price button
 button.addEventListener('click', ()=>{
     let squareMeters = document.getElementById('squareMetersInput').value;
-    calculateAdminAmount(squareMeters);
-    calculateCleaningAmout(squareMeters);
-    addMoreBills();
+    familyGenre();
+    addMoreBills(squareMeters);
 });
 
 function addFamilyMembers(){
-    let totalMembers;
     
     let artcileTest = document.createElement("article") //Documentación
     artcileTest.innerHTML = `
@@ -35,36 +33,14 @@ function addFamilyMembers(){
         <input type="number" placeholder="Ingrese la edad del nuevo miembro"> 
     `   
     familyMembersContainer.appendChild(artcileTest); //Documentación
-
-    totalMembers++;
-}
-
-function calculateAdminAmount(squareMetersParam) {
-    const totalAdminAmountCounter = document.getElementById('administrationValueAmountParaph');
-    if(land.options[land.selectedIndex].textContent === "Apartamento"){
-        `${totalAdminAmountCounter.innerHTML = (squareMetersParam*1500) + 50000}`
-    }else{
-        `${totalAdminAmountCounter.innerHTML = (squareMetersParam*1500) + 100000}`
-    }
-}
-
-
-function calculateCleaningAmout(squareMetersParam) {
-    const cleaningAmountCounter = document.getElementById('cleaningValueParaph')
-    if(land.options[land.selectedIndex].textContent === "Apartamento"){
-        `${cleaningAmountCounter.innerHTML = (((squareMetersParam*1500) + 50000)*0.10) + 
-                (squareMetersParam*1000) }`
-    }else{
-        `${cleaningAmountCounter.innerHTML = (((squareMetersParam*1500) + 100000)*0.10) + 
-            (squareMetersParam*1000) }`
-    }   
 }
 
 function familyGenre (){
+    let familyGymFinalPrice = 0;
     if(familyMembersGenre[familyMembersGenre.selectedIndex].textContent === "Masculino"){
-
+        
     }else{
-
+        console.log("Femenino");
     }
 }
 
@@ -73,29 +49,56 @@ function priceToPay(){
     `${totalBuildingObligationPrice.innerHTML = ``}`    
 }
 
-function addMoreBills() {
+function addMoreBills(squareMetersParams) {
     const amountCointainerLeftSide = document.createElement("section");
     const amountCointainerRightSide = document.createElement("section");
-    amountCointainerLeftSide.innerHTML = `
+
+    if (land.options[land.selectedIndex].textContent === "Apartamento") {
+        amountCointainerLeftSide.innerHTML += `
         <article class="leftSide">
             <h5>Señores apartamento #</h5>
             <small>Valor administración</small>
             <small>Valor cuota de aseo</small>
             <small>Derechos de gimnasio</small>
             <h5>Total a pagar</h5>
-        </article>
-    `
-    amountCointainerRightSide.innerHTML = `
+         </article>
+        `    
+        amountCointainerRightSide.innerHTML += `
             <article class="rightSide">
                 <h5>Cuota mes de</h5>
                 <class class="smallContainer">
-                    <small id="administrationValueAmountParaph">$ XXXXX</small>
-                    <small id="cleaningValueParaph">$ XXXXX</small>
+                    <small id="administrationValueAmountParaph">${(squareMetersParams*1500) + 50000}</small>
+                    <small id="cleaningValueParaph">${(((squareMetersParams*1500) + 100000)*0.10) + 
+                        (squareMetersParams*1000)}</small>
                     <small>$ XXXXX</small>
                 </class>
                 <h5 id="totalBuildingObligations">$ XXXXX</h5>
             </article>
-    `
+        `
+    }else{
+        amountCointainerLeftSide.innerHTML += `
+        <article class="leftSide">
+            <h5>Señores apartamento #</h5>
+            <small>Valor administración</small>
+            <small>Valor cuota de aseo</small>
+            <small>Derechos de gimnasio</small>
+            <h5>Total a pagar</h5>
+         </article>
+        `    
+        amountCointainerRightSide.innerHTML += `
+            <article class="rightSide">
+                <h5>Cuota mes de</h5>
+                <class class="smallContainer">
+                    <small id="administrationValueAmountParaph">${(squareMetersParams*1500) + 100000}</small>
+                    <small id="cleaningValueParaph">${(((squareMetersParams*1500) + 100000)*0.10) + 
+                        (squareMetersParams*1000)}</small>
+                    <small>$ XXXXX</small>
+                </class>
+                <h5 id="totalBuildingObligations">$ XXXXX</h5>
+            </article>
+        `
+    }
+    
     mainBillsContainer.appendChild(amountCointainerLeftSide);
     mainBillsContainer.appendChild(amountCointainerRightSide);
 }
