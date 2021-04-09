e99f10d90e0dc656a7f748952dedd1d4fc09236e
Juan David Y <juand8012@gmail.com>
2021-04-09 00:00:55 -0500

    Gym price isnt working well

--- src/index.js
+++ src/index.js
@@ -13,27 +13,33 @@ const mainBillsContainer = document.querySelector('.amountContainer');
 //Family members button
 familyMembersButton.addEventListener('click', addFamilyMembers);
 
+
 //It creates all the new elements
 button.addEventListener('click', () =>{
     const squareMeters = document.getElementById('squareMetersInput').value;
     const familyMembersAge = document.getElementById('familyMemberAge');
-    const testeando = familyGenre(familyMembersAge);
+    const gymCount = familyGenre(familyMembersAge);
     const gettingFamilyMembers = document.querySelectorAll('.gettingArticleFamily').length;
-    addMoreBills(squareMeters, testeando, gettingFamilyMembers);
-    console.log(gettingFamilyMembers);
+    addMoreBills(squareMeters, gymCount, gettingFamilyMembers);
 });
 
 function addFamilyMembers(){    
     let artcileTest = document.createElement("article");
-    artcileTest.className = "gettingArticleFamily"
+    artcileTest.className = "gettingArticleFamilyAux"
     artcileTest.innerHTML = `
         <select class="familyMemberGenre" name="FamilySelect">
             <option value="Masculino" selected>Masculino</option>
             <option value="Femenino">Femenino</option>
         </select>
         <input type="number" placeholder="Ingrese la edad del nuevo miembro"> 
+        <button type="submit" onClick="removeFamilyMember(event)" id="removeButton">-</button>
     `   
     familyMembersContainer.appendChild(artcileTest); 
+}
+
+function removeFamilyMember(event) {
+    console.log(event)
+    return event.target.parentElement.remove();
 }
 
 
@@ -79,10 +85,12 @@ function addMoreBills(squareMetersParams, gymCount, gettingFamilyMembers) {
     let totalGymMembers = gymCount * gettingFamilyMembers;
     let totalAdminHousePrice = (squareMetersParams*1500) + 100000;
     let totalCleaningHousePrice = (((squareMetersParams*1500) + 100000)*0.10) + (squareMetersParams*1000);
+    let actualDay = new Date ();
 
     //Price to pay
     let apartmentPriceToPay = totalAdminApartmentPrice + totalCleaningApartmentPrice + totalGymMembers;
-    let housePriceToPay = totalAdminHousePrice+totalCleaningHousePrice + totalGymMembers;
+    let housePriceToPay = totalAdminHousePrice + totalCleaningHousePrice + totalGymMembers;
+    console.log(apartmentPriceToPay);
 
     if (land.options[land.selectedIndex].textContent === "Apartamento") {
 
@@ -97,7 +105,7 @@ function addMoreBills(squareMetersParams, gymCount, gettingFamilyMembers) {
         `    
         amountCointainerRightSide.innerHTML += `
             <article class="rightSide">
-                <h5>Cuota mes de</h5>
+                <h5>Cuota del ${actualDay.toLocaleDateString()}</h5>
                 <class class="smallContainer">
                     <small id="administrationValueAmountParaph">$ ${totalAdminApartmentPrice}</small>
                     <small id="cleaningValueParaph">$ ${totalCleaningApartmentPrice}</small>
@@ -118,7 +126,7 @@ function addMoreBills(squareMetersParams, gymCount, gettingFamilyMembers) {
         `    
         amountCointainerRightSide.innerHTML += `
             <article class="rightSide">
-                <h5>Cuota mes de</h5>
+                <h5>Cuota del ${actualDay.toLocaleDateString()}</h5>
                 <class class="smallContainer">
                     <small id="administrationValueAmountParaph">$ ${totalAdminHousePrice}</small>
                     <small id="cleaningValueParaph">$ ${totalCleaningHousePrice}</small>
@@ -132,4 +140,6 @@ function addMoreBills(squareMetersParams, gymCount, gettingFamilyMembers) {
     mainBillsContainer.appendChild(amountCointainerRightSide);
 }
 
-
+function companyPriceToPay() {
+    
+}
